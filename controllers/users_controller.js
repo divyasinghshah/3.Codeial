17d2494993a79ceb05cb2b7aa8dd81f92dc7d2c0
divyasinghshah <divyasinghshah@gmail.com>
2022-01-18 11:19:42 +0530

    final commit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,24 @@
 const User=require('../models/user');
 
 module.exports.profile=function(req,res){
-    return res.render('users');
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(err){
+                console.log(err);
+                return;
+            }
+
+            if(user){
+                return res.render('user_profile',{
+                    title:user.name,
+                    user:user
+                });
+            }
+            
+
+            return res.redirect('/users/sign-in');
+        });
+    }
 }
 
 module.exports.signIn=function(req,res){
@@ -44,5 +61,27 @@ module.exports.create=function(req,res){
 }
 
 module.exports.createSession=function(req,res){
+
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log(err);
+            return;
+        }
+        if(user){
+            if(user.password!=req.body.password){
+                
+                return res.render('back');
+            }
+
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+
+
+        }else{
+           
+            return res.redirect('back');
+        }
+        
+    });
 
 }
