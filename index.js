b90185c01275dfd18f6796dcf764fea2af49c5f8
divyasinghshah <divyasinghshah@gmail.com>
2022-01-18 23:53:46 +0530

    passport setup

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const expressLayouts=require('express-ejs-layouts');
 const { ppid } = require('process');
 const db=require('./config/mongoose');
 const User=require('./models/user');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+const { Session } = require('express-session');
+// const MongoStore=require('connect-mongo');
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
@@ -14,6 +19,21 @@ app.use(cookie_parser());
 app.set('view engine','ejs');
 app.set('views','./views');
 app.set('layout extractStyles',true);
+
+app.use(session({
+    name:'codeial',
+    secret:'blahsomething',
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:(1000*60*100)
+    }
+    
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
 app.use('/',require('./routes'));
 
 app.listen(port,function(err){
