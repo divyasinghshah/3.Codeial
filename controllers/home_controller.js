1c20debd7b597519e69d75511dcb21849faac413
divyasinghshah <divyasinghshah@gmail.com>
2022-01-19 22:13:47 +0530

    comments section added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,16 +10,34 @@ module.exports.home=function(req,res){
     //     });
     // });
 
-    Post.find({}).populate('user').exec(function(err,posts){
+    // Post.find({}).populate('user').exec(function(err,posts){
+    //     if(err){
+    //         console.log(err);
+    //         return;
+    //     }
+    //     return res.render('home',{
+    //         title:"Codial | Home",
+    //         posts:posts
+    //     });
+    // });
+
+    Post.find({}).populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    }).exec(function(err,posts){
         if(err){
             console.log(err);
             return;
         }
-        return res.render('home',{
-            title:"Codial | Home",
+
+        res.render('home',{
+            title:'Codeial | Home',
             posts:posts
         });
-    })
+    });
     
    
     
