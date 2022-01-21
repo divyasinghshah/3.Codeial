20fb8a000bdc7b05ac0a69cbbaa33f4de43c6258
divyasinghshah <divyasinghshah@gmail.com>
2022-01-21 10:15:08 +0530

    async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,6 @@
 const Post=require('../models/post');
 const User=require('../models/user');
-module.exports.home=function(req,res){
+module.exports.home= async function(req,res){
 
     // Post.find({},function(err,posts){
     //     if(err){console.log(err); return;}
@@ -22,29 +22,35 @@ module.exports.home=function(req,res){
     //     });
     // });
 
-    Post.find({}).populate('user')
+ try{
+    let posts= await  Post.find({}).populate('user')
     .populate({
         path:'comments',
         populate:{
             path:'user'
         }
-    }).exec(function(err,posts){
-        if(err){
-            console.log(err);
-            return;
-        }
+    });
 
-        User.find({},function(err,users){
-            res.render('home',{
-                title:'Codeial | Home',
-                posts:posts,
-                all_users:users
-            });
+   
 
-        });
+    let users= await User.find({});
+
+                  return res.render('home',{
+                        title:'Codeial | Home',
+                        posts:posts,
+                        all_users:users
+                    });
 
         
-    });
+
+        
+ }
+
+ catch(err){
+     console.log(err);
+     return back('/');
+ }
+
     
    
     
